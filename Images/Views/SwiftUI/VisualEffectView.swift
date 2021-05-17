4eefc8e2a09ddd01ff104df69324176ab9908b3f
seeaya <connorbarnesdenver@icloud.com>
2021-05-17 16:02:36 -0600

    Added documentation.

--- Images/Views/SwiftUI/VisualEffectView.swift
+++ Images/Views/SwiftUI/VisualEffectView.swift
@@ -7,19 +7,27 @@
 
 import SwiftUI
 
+/// A view hosting an `NSVisualEffectView`.
 struct VisualEffectView: NSViewRepresentable {
+	/// A value indicating how the view’s contents blend with the surrounding content.
 	var effect: NSVisualEffectView.BlendingMode
+	/// The material shown by the visual effect view.
 	var material: NSVisualEffectView.Material
 	
 	func makeNSView(context: NSViewRepresentableContext<Self>) -> NSVisualEffectView {
 		NSVisualEffectView()
 	}
 	
-	func updateNSView(_ nsView: NSVisualEffectView, context: NSViewRepresentableContext<Self>) { nsView.blendingMode = effect
+	func updateNSView(
+		_ nsView: NSVisualEffectView,
+		context: NSViewRepresentableContext<Self>
+	) {
+		nsView.blendingMode = effect
 		nsView.material = material
 	}
 }
 
+// MARK:- Previews
 struct VisualEffectView_Previews: PreviewProvider {
 	static var previews: some View {
 		VisualEffectView(effect: .behindWindow,
