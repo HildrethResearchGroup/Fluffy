7c4c552c9f70876389c8e2b8fca59ef5a9ca3e9f
seeaya <connorbarnesdenver@icloud.com>
2021-06-10 22:58:14 -0600

    Implemented removing directories from the sidebar.

--- Images/Views/AppKit/Sidebar/Coordinator+FirstResponder.swift
+++ Images/Views/AppKit/Sidebar/Coordinator+FirstResponder.swift
@@ -49,7 +49,54 @@ extension SidebarViewController.Coordinator {
 	}
 	
 	func removeSelectedDirectories() {
+		let outlineView = controller.outlineView!
+		let selection = outlineView.selectedRowIndexes
+		guard !selection.isEmpty else { return }
+		let selectedDirectories = selection.compactMap { row in
+			// This should never return nil (all items in the sidebar should
+			// be directories) however if it does, skip over it, and it will be
+			// noted as an error in the following guard statement (we can't
+			// break out of a map)
+			return outlineView.item(atRow: row) as? Directory
+		}
 		
+		guard selectedDirectories.count == selection.count else {
+			print("NSOutlineView inconstancy: could not map all rows to directories")
+			return
+		}
+		
+		// Multiple items may be removed, so put everything in an update block to improve performance
+		outlineView.beginUpdates()
+		selectedDirectories.forEach { directory in
+			let parentDirectory = directory.parent
+			let childIndex = outlineView.childIndex(forItem: directory)
+			parent.viewContext.delete(directory)
+			
+			guard parentDirectory != nil else {
+				print("[WARNING] Error finding parent for directory item to be removed.")
+				return
+			}
+			guard childIndex != -1 else {
+				print("[WARNING] Error finding child index for subdirectory to be removed.")
+				return
+			}
+			
+			// Top level directories are a member of the root directory. However, NSOutlineView represents the item of the top level as nil, so if the parent is the root directory, change it to nil.
+			let outlineViewParent = parentDirectory == parent.rootDirectory
+				? nil
+				: parent
+			
+			outlineView.removeItems(at: IndexSet(integer: childIndex),
+															inParent: outlineViewParent,
+															withAnimation: .slideDown)
+		}
+		outlineView.endUpdates()
+		
+		do {
+			try parent.viewContext.save()
+		} catch {
+			print("[WARNING] Failed to save context: \(error)")
+		}
 	}
 }
 
